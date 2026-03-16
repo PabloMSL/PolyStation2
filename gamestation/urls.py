2f648b28baefa445ec7116ce17790f56ceb6661c
Santiago-Beltran1 <pedrazasantiago837@gmail.com>
2026-03-16 10:12:33 -0500

    Se añaden las urls de los distribuidores

--- gamestation/urls.py
+++ gamestation/urls.py
@@ -15,3 +15,14 @@ path('login/', login_comprador, name='login_comprador'),
 path('biblioteca/', biblioteca, name='biblioteca'),
 path('comprar/<str:juego_id>/', comprar_juego, name='comprar_juego'),
 path('resena/<str:juego_id>/', crear_resena, name='crear_resena'),
+
+# DISTRIBUIDOR
+
+path('registro-distribuidor/', registro_distribuidor, name='registro_distribuidor'),
+path('login-distribuidor/', login_distribuidor, name='login_distribuidor'),
+path('dashboard/', dashboard_distribuidor, name='dashboard_distribuidor'),
+path('mis-juegos/', listar_juegos_distribuidor, name='listar_juegos_distribuidor'),
+path('crear-juego/', crear_juego, name='crear_juego'),
+path('editar-juego/<str:juego_id>/', editar_juego, name='editar_juego'),
+path('eliminar-juego/<str:juego_id>/', eliminar_juego, name='eliminar_juego'),
+]
