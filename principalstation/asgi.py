41a013034730ecef4b0c4736885a7e960ed2249c
PabloMSL <jpablomilitar@gmail.com>
2026-03-25 11:46:22 -0500

    hoy

--- principalstation/asgi.py
+++ principalstation/asgi.py
@@ -14,3 +14,23 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'principalstation.settings')
 
 application = get_asgi_application()
+
+
+# 1.PERMITI peticiones http
+
+django_asgi_app = get_asgi_application()
+
+#2. Permitir canales y websockets
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from gamestation.routin import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
